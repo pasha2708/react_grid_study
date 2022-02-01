36fec1d7036e058f06c8f13a48444ef91c4c52f8
Pavlo Pysaniuk <93548330+pasha2708@users.noreply.github.com>
2022-02-01 19:08:57 +0200

    React Grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import { EditingState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableEditRow,
+  TableEditColumn,
+} from '@devexpress/dx-react-grid-material-ui';
 
-function App() {
+import {
+  generateRows,
+  defaultNestedColumnValues,
+} from './demo-data/generator';
+
+const getRowId = row => row.id;
+
+const App = () => {
+  const [columns] = useState([
+    {
+      name: 'firstName',
+      title: 'First Name',
+      getCellValue: row => (row.user ? row.user.firstName : undefined),
+    },
+    {
+      name: 'lastName',
+      title: 'Last Name',
+      getCellValue: row => (row.user ? row.user.lastName : undefined),
+    },
+    {
+      name: 'car',
+      title: 'Car',
+      getCellValue: row => (row.car ? row.car.model : undefined),
+    },
+    { name: 'position', title: 'Position' },
+    { name: 'city', title: 'City' },
+  ]);
+  const [rows, setRows] = useState(generateRows({
+    columnValues: { id: ({ index }) => index, ...defaultNestedColumnValues },
+    length: 8,
+  }));
+  const [editingColumnExtensions] = useState([
+    {
+      columnName: 'firstName',
+      createRowChange: (row, value) => ({ user: { ...row.user, firstName: value } }),
+    },
+    {
+      columnName: 'lastName',
+      createRowChange: (row, value) => ({ user: { ...row.user, lastName: value } }),
+    },
+    {
+      columnName: 'car',
+      createRowChange: (row, value) => ({ car: { model: value } }),
+    },
+  ]);
+
+  const commitChanges = ({ added, changed, deleted }) => {
+    let changedRows;
+    if (added) {
+      const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
+      changedRows = [
+        ...rows,
+        ...added.map((row, index) => ({
+          id: startingAddedId + index,
+          ...row,
+        })),
+      ];
+    }
+    if (changed) {
+      changedRows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
+    }
+    if (deleted) {
+      const deletedSet = new Set(deleted);
+      changedRows = rows.filter(row => !deletedSet.has(row.id));
+    }
+    setRows(changedRows);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper>
+      <Grid
+        rows={rows}
+        columns={columns}
+        getRowId={getRowId}
+      >
+        <EditingState
+          columnExtensions={editingColumnExtensions}
+          onCommitChanges={commitChanges}
+        />
+        <Table />
+        <TableHeaderRow />
+        <TableEditRow />
+        <TableEditColumn showAddCommand showEditCommand showDeleteCommand  />
+      </Grid>
+    </Paper>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
 ReactDOM.render(
   <React.StrictMode>
